f048e11e4a3dc75ddf3b28e37ad29a31550ead2d
Björn Holm <bjh@kth.se>
2018-12-09 17:28:03 +0100

    +day09 part2

--- Day09.scala
+++ Day09.scala
@@ -1,20 +1,27 @@
 import scala.collection.mutable
 
 object Day09 extends App {
-  def day09(playerCount: Int, marbleCount: Int): Int = {
-    val scores = new Array[Int](playerCount)
-    val circle = new mutable.ListBuffer[Int]
-    circle += 0
-    var curr = 0
+  def day09(playerCount: Int, marbleCount: Int): Long = {
+    val scores = new Array[Long](playerCount)
 
-    for (marble <- 1 to marbleCount) {
+    var circle = mutable.DoubleLinkedList(0,1)
+    circle.next.next = circle
+
+    for (marble <- 2 to marbleCount) {
       if (marble%23 == 0) {
-        curr = (curr - 7 + circle.length) % circle.length
-        scores(marble % playerCount) += marble + circle.remove(curr)
+        circle = circle.prev.prev.prev.prev.prev.prev.prev
+        scores(marble % playerCount) += marble + circle.head
+        circle.remove
+        circle = circle.next
       }
       else {
-        curr = (curr + 2) % circle.length
-        circle.insert(curr, marble)
+        circle = circle.next
+        val tmp = mutable.DoubleLinkedList(marble)
+        tmp.next = circle.next
+        circle.next.prev = tmp
+        circle.next = tmp
+        tmp.prev = circle
+        circle = circle.next
       }
     }
     scores.max
@@ -22,4 +29,7 @@ object Day09 extends App {
 
   val part1 = day09(410, 72059)
   println(part1)
+
+  val part2 = day09(410, 7205900)
+  println(part2)
 }
